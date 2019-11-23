5cd0122724e1d96f7cbcee9d3de5271e180d012d
Daniel Hernandez <dhdan97@gmail.com>
2019-11-23 17:51:38 -0500

    got save and exit button working

--- Assignment5/src/application/DirectoryController.java
+++ Assignment5/src/application/DirectoryController.java
@@ -1,13 +1,27 @@
 package application;
 
 
+import java.io.File;
+import java.util.Optional;
+
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
 
 public class DirectoryController {
 
@@ -24,10 +38,10 @@ public class DirectoryController {
     private Button btnNavDel;
 
     @FXML
-    private Button btnNavDel1;
+    private Button btnNavAdd;
 
     @FXML
-    private Button btnNavNext1;
+    private Button btnNavPrev;
 
     @FXML
     private Button btnNavNext;
@@ -51,8 +65,33 @@ public class DirectoryController {
     private Button btnExit;
 
     @FXML
-    void buttonClicked(ActionEvent event) {
-    	FileChooser filechooser = new FileChooser();
+    void loadButtonClicked(ActionEvent event) {
+    	txtFldName.setDisable(false);
+    	txtFldName1.setDisable(false);
+    	txtFldName2.setDisable(false);
+    	btnNavPrev.setDisable(false);
+    	btnNavNext.setDisable(false);
+    	btnNavDel.setDisable(false);
+    	btnNavAdd.setDisable(false);
+    	btnSerialize.setDisable(false);
+    	
+    	FileChooser fileChooser = new FileChooser();
+    	File selectedFile = fileChooser.showOpenDialog(null);
     }
+    
+    @FXML
+    void exitButtonClicked(ActionEvent event) {      
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("Are you sure you want to exit?");
+    	
+    	Optional<ButtonType> result = alert.showAndWait();
 
+    	if(result.get() == ButtonType.OK) {
+    		Platform.exit();
+    	}
+    	else if(result.get() == ButtonType.CANCEL) {
+    		alert.close();
+    	}
+    }
 }
